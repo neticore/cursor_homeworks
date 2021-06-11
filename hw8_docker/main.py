596bb48f01a52efefbbfe364b4aedd28fbcc1d18
neticore <zakencore@gmail.com>
2021-06-11 22:33:39 +0300

    add flask new

---
+++ hw8_docker/main.py
@@ -0,0 +1,22 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def start():
+    return "Start page"
+
+
+@app.route('/home')
+def home():
+    return "This is Homepage"
+
+
+@app.route('/news')
+def newsp():
+    return "News PAge"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
